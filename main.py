d63a36b6207688abd5247c3947ebc956287ad0b5
yoni13 <76271912+yoni13@users.noreply.github.com>
2022-10-11 18:53:34 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fake_useragent import UserAgent
 ua = UserAgent()
 from faker import Faker  
 faker = Faker()
-passwd = 200000
+passwd = 205459
 
 while True:
 	header = {'User-Agent':ua.chrome,'Upgrade-Insecure-Requests':'1','Referer':'http://140.126.151.12/csnskj/Permain.asp','Origin':'http://140.126.151.12','Host':'140.126.151.12','Content-Type':'application/x-www-form-urlencoded','Content-Length':'169','Connection':'keep-alive','Cache-Control':'max-age=0','Accept-Language':'zh-TW,zh;q=0.9,en-US;q=0.8,en;q=0.7','Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9','Accept-Encoding': 'gzip, deflate'}
